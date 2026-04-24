f9f98965a7f239225a5636eb21f29338a07f1d73
hw24a047-afk <hw24a047@oecu.jp>
2026-04-24 10:27:37 +0900

    変数

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int x = 17;
+    void Start()
+    {
+        x = 10;
+        x = 12;
+        Debug.Log (x);
+    }
+
+    void Update()
+    {
+        
+    }
+}
